8e69afa339d6305969098a60005c5f3d7bf737e3
FeverDreme <jeffreyhuang08@gmail.com>
2021-04-20 23:01:52 -0400

    classes string for css

--- src/components/CardImg.tsx
+++ src/components/CardImg.tsx
@@ -5,20 +5,23 @@ export interface Props {
     title: string;
     desc: string;
     alt?: string;
+
+    // css
+    classes?: string;
 };
 
 const CardImg: React.FC<Props> = (props) => {
-    let {img, title, desc, alt} = props;
+    let {img, title, desc, alt, classes} = props;
     alt = alt === undefined ? "image" : alt;
+    classes = classes !== undefined ? "card " + classes : "card";
 
     return (
-        <div className="card">
+        <div className={classes}>
             <img className="card-img-top" src={img} alt={alt}/>
 
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{desc}</p>
-                <button className="btn btn-primary">Go somewhere</button>
             </div>
         </div>
     );
