e0eb23e504f1779e2f71c515b990d85a93dc9f19
FeverDreme <jeffreyhuang08@gmail.com>
2021-04-20 23:01:19 -0400

    refactored for path matching

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './Navbar'
+import Navbar from 'components/Navbar'
 
 export default function Banner() {
     return (
@@ -8,10 +8,12 @@ export default function Banner() {
                 <div className="jumbotron pb-0">
                     <div className="row">
                         <div className="col-10">
+                            {/* Title and Subtitle */}
                             <h1 className="mw-50">Flying Cow Organics</h1>
                             <p className="lead mw-50">Adipisicing pariatur elit sunt officia aute laborum.</p>
                         </div>
 
+                        {/* Image */}
                         <div className="col-2 image-container">
                             <img src="./logo.jpg" className="rounded-circle" alt="flying cow logo"/>
                         </div>
